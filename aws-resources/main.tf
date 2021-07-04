0108d026b958358b55675cdf4cef3671d57b355c
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-07-04 22:08:33 +0900

    add terraform resources.

---
+++ aws-resources/main.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  type = string
+  description = "AWS Region, where to deploy ELK cluster."
+  default = "ap-northeast-1"
+}
+
+locals {
+  common_prefix = "spring-elk"
+  elk_domain = "${local.common_prefix}-elk-domain"
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+provider "aws" {
+  region = var.region
+}
